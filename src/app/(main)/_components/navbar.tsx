c9f382771d87b47b46cf47e8de06537e9df2075e
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-09-03 15:19:29 +0300

    Created the navbar component

---
+++ src/app/(main)/_components/navbar.tsx
@@ -0,0 +1,19 @@
+import UserButton from '@/components/user-button'
+import Link from 'next/link'
+
+type Props = {}
+
+const NavBar = () => {
+	return (
+		<header className='bg-card sticky top-0 z-10 shadow-sm'>
+			<div className='max-w-7xl flex flex-wrap items-center justify-center gap-5 px-5 py-3'>
+				<Link href='/' className='text-primary text-2xl font-bold'>
+					BugBook
+				</Link>
+				<UserButton />
+			</div>
+		</header>
+	)
+}
+
+export default NavBar
