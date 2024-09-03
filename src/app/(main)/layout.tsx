18a3a40a1df6f3f7cc4fdbbf5fd111729026c0d3
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-09-03 14:53:38 +0300

    Added some styling for the layout children

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -2,6 +2,7 @@ import { validateRequest } from '@/auth'
 import { redirect } from 'next/navigation'
 import { ReactNode } from 'react'
 import SessionProvider from './_components/session-provider'
+import NavBar from './_components/navbar'
 
 type Props = {
 	children: ReactNode
@@ -12,7 +13,14 @@ const MainLayout = async ({ children }: Props) => {
 
 	if (!session.user) redirect('/login')
 
-	return <SessionProvider value={session}>{children}</SessionProvider>
+	return (
+		<SessionProvider value={session}>
+			<div className='flex flex-col h-full'>
+				<NavBar />
+				<div className='max-w-7xl p-5 mx-auto'>{children}</div>
+			</div>
+		</SessionProvider>
+	)
 }
 
 export default MainLayout
