c8e254d5a2f2d311b5f433b5a1a16474e04d5f91
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-09-03 12:26:54 +0300

    Updated the login page

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -14,9 +14,15 @@ const LoginPage = () => {
 		<main className='flex items-center justify-center h-full p-5'>
 			<div className='flex h-full w-full max-h-[40rem] max-w-[64rem] overflow-hidden rounded-2xl bg-card shadow-2xl'>
 				<div className='md:w-1/2 w-full p-10 space-y-10 overflow-y-auto'>
-					<h1 className='text-3xl font-bold text-center'>
-						Login to BugBook
-					</h1>
+					<div className='space-y-1 text-center'>
+						<h1 className='text-3xl font-bold'>Login to BugBook</h1>
+						<p className='text-muted-foreground'>
+							A place where event{' '}
+							<span className='italic'>you</span> can find a
+							friend
+						</p>
+					</div>
+
 					<div className='space-y-5'>
 						<LoginForm />
 						<Link
