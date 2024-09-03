f8cbff47534cdde5b3d899b475d06983446aad2f
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-09-03 10:59:16 +0300

    Added the toaster component to the application

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import QueryProvider from '@/providers/query-provider'
 import ThemeProvider from '@/providers/theme-provider'
+import { Toaster } from '@/components/ui/sonner'
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -23,7 +24,10 @@ export default function RootLayout({
 		<html lang='en'>
 			<body className={inter.className}>
 				<QueryProvider>
-					<ThemeProvider>{children}</ThemeProvider>
+					<ThemeProvider>
+						<Toaster />
+						{children}
+					</ThemeProvider>
 				</QueryProvider>
 			</body>
 		</html>
