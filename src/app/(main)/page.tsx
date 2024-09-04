cc5e7e73726acb46173f9518e80948791d8e7a16
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-09-04 15:52:01 +0300

    Replaced the query function with teh for you feed component

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,36 +1,13 @@
 import PostEditor from '@/components/posts/editor/post-editor'
-import Post from '@/components/posts/post'
 import TrendsSidebar from '@/components/trends-sidebar'
-import { db } from '@/db'
-import { Posts } from '@/db/schema'
-import { desc } from 'drizzle-orm'
+import ForYouFeed from './_components/foryou-feed'
 
-const HomePage = async () => {
-	const posts = await db.query.Posts.findMany({
-		columns: {
-			id: true,
-			content: true,
-			createdAt: true
-		},
-		with: {
-			user: {
-				columns: {
-					username: true,
-					displayName: true,
-					avatarUrl: true
-				}
-			}
-		},
-		orderBy: [desc(Posts.createdAt)]
-	})
-
+const HomePage = () => {
 	return (
 		<main className='flex w-full min-w-0 gap-5'>
 			<div className='w-full min-w-0 space-y-5'>
 				<PostEditor />
-				{posts.map((post) => (
-					<Post key={post.id} post={post} />
-				))}
+				<ForYouFeed />
 			</div>
 			<TrendsSidebar />
 		</main>
