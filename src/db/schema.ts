b2094d46972d71235f04e9b2c5f07c42178a56c3
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-09-04 10:54:50 +0300

    Added the posts table to the database

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,5 +1,5 @@
 import { relations } from 'drizzle-orm'
-import { pgTable, text, timestamp, varchar } from 'drizzle-orm/pg-core'
+import { pgTable, text, timestamp, uuid, varchar } from 'drizzle-orm/pg-core'
 
 export const userTable = pgTable('users', {
 	id: text('id').primaryKey(),
@@ -10,20 +10,13 @@ export const userTable = pgTable('users', {
 	googleId: varchar('googleId', { length: 255 }).unique(),
 	avatarUrl: varchar('avatar_url', { length: 512 }),
 	bio: text('bio'),
-	createdAt: timestamp('created_at', {
-		withTimezone: true,
-		mode: 'date'
-	})
-		.defaultNow()
-		.notNull(),
-	updatedAt: timestamp('updated_at', {
-		withTimezone: true,
-		mode: 'date'
-	}).$onUpdate(() => new Date())
+	createdAt: timestamp('created_at').defaultNow().notNull(),
+	updatedAt: timestamp('updated_at').$onUpdate(() => new Date())
 })
 
 export const userTableRelations = relations(userTable, ({ many }) => ({
-	sessions: many(sessionTable)
+	sessions: many(sessionTable),
+	posts: many(Posts)
 }))
 
 export const sessionTable = pgTable('sessions', {
@@ -31,15 +24,29 @@ export const sessionTable = pgTable('sessions', {
 	userId: text('user_id')
 		.notNull()
 		.references(() => userTable.id, { onDelete: 'cascade' }),
-	expiresAt: timestamp('expires_at', {
-		withTimezone: true,
-		mode: 'date'
-	}).notNull()
+	expiresAt: timestamp('expires_at').notNull()
 })
 
 export const sessionRelations = relations(sessionTable, ({ one }) => ({
 	user: one(userTable, {
 		fields: [sessionTable.userId],
+		references: [userTable.id]
+	})
+}))
+
+export const Posts = pgTable('posts', {
+	id: uuid('id').defaultRandom().primaryKey(),
+	content: text('content').notNull(),
+	userId: text('user_id').references(() => userTable.id, {
+		onDelete: 'cascade'
+	}),
+	createdAt: timestamp('created_at').defaultNow().notNull(),
+	updatedAt: timestamp('updated_at').$onUpdate(() => new Date())
+})
+
+export const postRelations = relations(Posts, ({ one }) => ({
+	user: one(userTable, {
+		fields: [Posts.userId],
 		references: [userTable.id]
 	})
 }))
