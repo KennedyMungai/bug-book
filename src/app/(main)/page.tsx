09b91238f647541ece8d6c94992351d4e3b775ff
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-09-04 12:27:33 +0300

    Created the trends sidebar component and rendered it to the index page of the application

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,5 +1,6 @@
 import PostEditor from '@/components/posts/editor/post-editor'
 import Post from '@/components/posts/post'
+import TrendsSidebar from '@/components/trends-sidebar'
 import { db } from '@/db'
 import { Posts } from '@/db/schema'
 import { desc } from 'drizzle-orm'
@@ -24,13 +25,14 @@ const HomePage = async () => {
 	})
 
 	return (
-		<main className='w-full min-w-0'>
+		<main className='flex w-full min-w-0 gap-5'>
 			<div className='w-full min-w-0 space-y-5'>
 				<PostEditor />
 				{posts.map((post) => (
 					<Post key={post.id} post={post} />
 				))}
 			</div>
+			<TrendsSidebar />
 		</main>
 	)
 }
