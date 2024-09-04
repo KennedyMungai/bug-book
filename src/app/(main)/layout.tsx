fa2491f83fe5542a2e7b3e33d82e359b384b71a6
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-09-04 10:42:09 +0300

    Added some styles for the div containing the children and the sidebar

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -3,6 +3,7 @@ import { redirect } from 'next/navigation'
 import { ReactNode } from 'react'
 import SessionProvider from './_components/session-provider'
 import NavBar from './_components/navbar'
+import MenuBar from './_components/menu-bar'
 
 type Props = {
 	children: ReactNode
@@ -17,7 +18,10 @@ const MainLayout = async ({ children }: Props) => {
 		<SessionProvider value={session}>
 			<div className='flex flex-col h-full'>
 				<NavBar />
-				<div className='max-w-7xl p-5 mx-auto'>{children}</div>
+				<div className='max-w-7xl p-5 mx-auto flex w-full grow gap-5'>
+					<MenuBar className='sticky top-[5.25rem] h-fit hidden sm:block flex-none space-y-3 rounded-2xl bg-card px-3 py-5 lg:px-5 shadow-sm xl:w-80' />
+					{children}
+				</div>
 			</div>
 		</SessionProvider>
 	)
