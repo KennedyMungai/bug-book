6cf3a9a94b27d0dea7ebdc6abcca404ca19e0dde
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-09-04 14:04:07 +0300

    Fetching the data for who to follow

--- src/components/trends-sidebar.tsx
+++ src/components/trends-sidebar.tsx
@@ -3,7 +3,9 @@ import { Skeleton } from '@/components/ui/skeleton'
 import { db } from '@/db'
 import { userTable } from '@/db/schema'
 import { eq, not } from 'drizzle-orm'
+import Link from 'next/link'
 import { Suspense } from 'react'
+import UserAvatar from '@/components/user-avatar'
 
 type Props = {}
 
@@ -40,6 +42,27 @@ const WhoToFollow = async () => {
 	return (
 		<div className='rounded-2xl bg-card p-5 space-y-5 shadow-sm'>
 			<div className='text-xl font-bold'>Who to follow</div>
+			{usersToFollow.map((user) => (
+				<div
+					key={user.id}
+					className='flex items-center justify-between gap-2'
+				>
+					<Link
+						href={`/users/${user.username}`}
+						className='flex items-center gap-3'
+					>
+						<UserAvatar avatarUrl={user.avatarUrl} />
+						<div>
+							<p className='line-clamp-1 hover:underline font-semibold break-all'>
+								{user.displayName}
+							</p>
+							<p className='line-clamp-1 text-muted-foreground break-all'>
+								@{user.username}
+							</p>
+						</div>
+					</Link>
+				</div>
+			))}
 		</div>
 	)
 }
