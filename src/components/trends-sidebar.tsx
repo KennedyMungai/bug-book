dacceb346fbfc2b6a9ce261ee9b35b3fc07da25b
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-09-04 12:46:03 +0300

    Made some updates to the trends sidebar pending state

--- src/components/trends-sidebar.tsx
+++ src/components/trends-sidebar.tsx
@@ -1,11 +1,58 @@
+import { validateRequest } from '@/auth'
+import { Skeleton } from '@/components/ui/skeleton'
+import { db } from '@/db'
+import { userTable } from '@/db/schema'
+import { eq, not } from 'drizzle-orm'
+import { Suspense } from 'react'
+
 type Props = {}
 
 const TrendsSidebar = () => {
 	return (
 		<div className='sticky top-[5.25rem] hidden md:block lg:w-80 w-72 h-fit flex-none space-y-5'>
-			TrendsSidebar
+			<Suspense fallback={<WhoToFollowFallback />}>
+				<WhoToFollow />
+			</Suspense>
 		</div>
 	)
 }
 
 export default TrendsSidebar
+
+const WhoToFollow = async () => {
+	const { user } = await validateRequest()
+
+	if (!user) return
+
+	await new Promise((r) => setTimeout(r, 20000))
+
+	const usersToFollow = await db.query.userTable.findMany({
+		columns: {
+			id: true,
+			username: true,
+			displayName: true,
+			avatarUrl: true
+		},
+		where: not(eq(userTable.id, user.id)),
+		limit: 5
+	})
+
+	return (
+		<div className='rounded-2xl bg-card p-5 space-y-5 shadow-sm'>
+			<div className='text-xl font-bold'>Who to follow</div>
+		</div>
+	)
+}
+
+const WhoToFollowFallback = () => {
+	return (
+		<div className='rounded-2xl bg-card p-5 space-y-5 shadow-sm'>
+			<div className='text-xl font-bold'>Who to follow</div>
+			<Skeleton className='rounded-2xl w-full h-8' />
+			<Skeleton className='rounded-2xl w-full h-8' />
+			<Skeleton className='rounded-2xl w-full h-8' />
+			<Skeleton className='rounded-2xl w-full h-8' />
+			<Skeleton className='rounded-2xl w-full h-8' />
+		</div>
+	)
+}
