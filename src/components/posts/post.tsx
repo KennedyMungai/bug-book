3372427b3464417b6c510dc6c2118c2cf1f2df9d
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-09-04 12:18:23 +0300

    Rendering the post contents

--- src/components/posts/post.tsx
+++ src/components/posts/post.tsx
@@ -1,3 +1,7 @@
+import Link from 'next/link'
+import UserAvatar from '@/components/user-avatar'
+import { formatRelativeDate } from '@/lib/utils'
+
 type Props = {
 	post: {
 		id: string
@@ -12,7 +16,32 @@ type Props = {
 }
 
 const Post = ({ post }: Props) => {
-	return <article>{post.content}</article>
+	return (
+		<article className='space-y-3 rounded-2xl bg-card p-5 shadow-sm'>
+			<div className='flex flex-wrap gap-3'>
+				<Link href={`/users/${post.user?.username}`}>
+					<UserAvatar avatarUrl={post.user?.avatarUrl} />
+				</Link>
+				<div>
+					<Link
+						href={`/users/${post.user?.username}`}
+						className='block font-medium hover:underline'
+					>
+						{post.user?.displayName}
+					</Link>
+					<Link
+						href={`/posts/${post.id}`}
+						className='block text-sm text-muted-foreground hover:underline'
+					>
+						{formatRelativeDate(post.createdAt)}
+					</Link>
+				</div>
+			</div>
+			<div className='whitespace-pre-line break-words'>
+				{post.content}
+			</div>
+		</article>
+	)
 }
 
 export default Post
