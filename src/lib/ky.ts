131505843efad268036232e08146f46162355c90
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-09-05 09:31:24 +0300

    Created the file containing the ky instance

---
+++ src/lib/ky.ts
@@ -0,0 +1,15 @@
+import ky from 'ky'
+
+const kyInstance = ky.create({
+	parseJson(text) {
+		JSON.parse(text, (key, value) => {
+			if (key.endsWith('At') && typeof value === 'string') {
+				return new Date(value)
+			}
+
+			return value
+		})
+	}
+})
+
+export default kyInstance
