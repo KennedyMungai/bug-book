95d7f932c6c39960933aa9abdc1436c21a1c2b00
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-09-05 11:45:10 +0300

    Proper data fetching

--- src/components/posts/editor/actions.ts
+++ src/components/posts/editor/actions.ts
@@ -2,27 +2,43 @@
 
 import { validateRequest } from '@/auth'
 import { db } from '@/db'
-import { Posts, userTable } from '@/db/schema'
+import { Posts } from '@/db/schema'
 import { createPostsSchema } from '@/lib/validation'
+import { eq } from 'drizzle-orm'
 
 export const submitPost = async (input: string) => {
-	const { session, user } = await validateRequest()
+	const { user } = await validateRequest()
 
 	if (!user) throw new Error('Unauthorized')
 
 	const { content } = createPostsSchema.parse({ content: input })
 
-	const post = await db
+	const [postId] = await db
 		.insert(Posts)
 		.values({ content, userId: user.id })
 		.returning({
-			id: Posts.id,
-			content: Posts.content,
-			createdAt: Posts.createdAt,
-			userDisplayName: userTable.displayName,
-			userUsername: userTable.username,
-			userAvatarUrl: userTable.avatarUrl
+			id: Posts.id
 		})
+
+	const post = await db.query.Posts.findFirst({
+		columns: {
+			id: true,
+			content: true,
+			createdAt: true
+		},
+		with: {
+			user: {
+				columns: {
+					username: true,
+					displayName: true,
+					avatarUrl: true
+				}
+			}
+		},
+		where: eq(Posts.id, postId.id)
+	})
+
+	if (!post) throw new Error('Post not found')
 
 	return post
 }
