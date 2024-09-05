d3312bfee9b730db3a7c18ffc0d4cf1c33c51154
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-09-05 10:14:18 +0300

    Created the infinite scroll container

---
+++ src/components/infinite-scroll-container.tsx
@@ -0,0 +1,29 @@
+import { PropsWithChildren } from 'react'
+import { useInView } from 'react-intersection-observer'
+
+interface Props extends PropsWithChildren {
+	onBottomReached: () => void
+	className?: string
+}
+
+const InfiniteQueryContainer = ({
+	onBottomReached,
+	children,
+	className
+}: Props) => {
+	const { ref } = useInView({
+		rootMargin: '200px',
+		onChange: (inView) => {
+			if (inView) onBottomReached()
+		}
+	})
+
+	return (
+		<div className={className}>
+			{children}
+			<div ref={ref} />
+		</div>
+	)
+}
+
+export default InfiniteQueryContainer
