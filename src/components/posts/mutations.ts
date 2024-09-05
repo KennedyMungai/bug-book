d2d65d505d35869c146f465772e5fed273375e9b
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-09-05 12:27:14 +0300

    Created the mutations file containing the call to delete a post

---
+++ src/components/posts/mutations.ts
@@ -0,0 +1,58 @@
+import { PostData, PostsPage } from '@/lib/types'
+import {
+	InfiniteData,
+	QueryFilters,
+	useMutation,
+	useQueryClient
+} from '@tanstack/react-query'
+import { usePathname, useRouter } from 'next/navigation'
+import { toast } from 'sonner'
+import { deletePost } from './actions'
+
+export const useDeletePostMutation = (post: PostData) => {
+	const queryClient = useQueryClient()
+
+	const router = useRouter()
+
+	const pathname = usePathname()
+
+	const mutation = useMutation({
+		mutationFn: deletePost,
+		onSuccess: async (deletedPost) => {
+			const queryFilter: QueryFilters = {
+				queryKey: ['post-feed']
+			}
+
+			await queryClient.cancelQueries(queryFilter)
+
+			queryClient.setQueriesData<InfiniteData<PostsPage, string | null>>(
+				queryFilter,
+				(oldData) => {
+					if (!oldData) return { pages: [], pageParams: [] }
+
+					return {
+						pageParams: oldData?.pageParams,
+						pages: oldData.pages.map((page) => ({
+							nextCursor: page.nextCursor,
+							posts: page.posts.filter(
+								(p) => p.id !== deletedPost.id
+							)
+						}))
+					}
+				}
+			)
+
+			toast.success('Post deleted successfully')
+
+			if (pathname === `/posts/${deletedPost.id}`)
+				router.push(`/users/${deletedPost.user?.username}`)
+		},
+		onError: (error) => {
+			console.error(error.message)
+
+			toast.error('Failed to delete the message. Please try again')
+		}
+	})
+
+	return mutation
+}
