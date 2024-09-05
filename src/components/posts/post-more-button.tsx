e1b57d01d46a514e1eb025372453e82236203b02
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-09-05 12:49:07 +0300

    Created the button that has the dropdown that handles the delete post logic

---
+++ src/components/posts/post-more-button.tsx
@@ -0,0 +1,37 @@
+import { Button } from '@/components/ui/button'
+import {
+	DropdownMenu,
+	DropdownMenuTrigger
+} from '@/components/ui/dropdown-menu'
+import { PostData } from '@/lib/types'
+import { MoreHorizontalIcon } from 'lucide-react'
+import { useState } from 'react'
+import DeletePostsDialog from './delete-posts-dialog'
+
+type Props = {
+	post: PostData
+	className?: string
+}
+
+const PostMoreButton = ({ post, className }: Props) => {
+	const [showDeleteDialog, setShowDeleteDialog] = useState(false)
+
+	return (
+		<>
+			<DropdownMenu>
+				<DropdownMenuTrigger asChild>
+					<Button size='icon' variant={'ghost'} className={className}>
+						<MoreHorizontalIcon className='size-5 text-muted-foreground' />
+					</Button>
+				</DropdownMenuTrigger>
+			</DropdownMenu>
+			<DeletePostsDialog
+				post={post}
+				open={showDeleteDialog}
+				onClose={() => setShowDeleteDialog(false)}
+			/>
+		</>
+	)
+}
+
+export default PostMoreButton
