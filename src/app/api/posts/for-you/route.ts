cae9458098098ead59de6a66491eaf4a2368dfba
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-09-05 09:50:31 +0300

    Fetching the paginated data and the nextCursor

--- src/app/api/posts/for-you/route.ts
+++ src/app/api/posts/for-you/route.ts
@@ -1,11 +1,16 @@
 import { validateRequest } from '@/auth'
 import { db } from '@/db'
 import { Posts } from '@/db/schema'
-import { desc } from 'drizzle-orm'
-import { NextResponse } from 'next/server'
+import { PostsPage } from '@/lib/types'
+import { desc, gt } from 'drizzle-orm'
+import { NextRequest, NextResponse } from 'next/server'
 
-export const GET = async (req: Request) => {
+export const GET = async (req: NextRequest) => {
 	try {
+		const cursor = req.nextUrl.searchParams.get('cursor') || undefined
+
+		const pageSize = 10
+
 		const { user } = await validateRequest()
 
 		if (!user)
@@ -26,10 +31,20 @@ export const GET = async (req: Request) => {
 					}
 				}
 			},
+			where: cursor ? gt(Posts.id, cursor) : undefined,
+			limit: pageSize + 1,
 			orderBy: [desc(Posts.createdAt)]
 		})
 
-		return NextResponse.json(posts)
+		const nextCursor =
+			posts.length > pageSize ? posts[posts.length - 1].id : undefined
+
+		const data: PostsPage = {
+			posts: posts.slice(0, pageSize),
+			nextCursor
+		}
+
+		return NextResponse.json(data)
 	} catch (error: any) {
 		console.error(error.message)
 		return NextResponse.json(error.message, { status: 500 })
