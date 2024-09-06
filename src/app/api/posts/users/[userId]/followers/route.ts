fb509e9a6dd72fdf8fad603ed202f946ca363adb
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-09-06 07:45:14 +0300

    Created the user followers route

---
+++ src/app/api/posts/users/[userId]/followers/route.ts
@@ -0,0 +1,51 @@
+import { validateRequest } from '@/auth'
+import { db } from '@/db'
+import { userTable } from '@/db/schema'
+import { FollowerInfo } from '@/lib/types'
+import { eq } from 'drizzle-orm'
+import { NextRequest, NextResponse } from 'next/server'
+
+export const GET = async (
+	req: NextRequest,
+	{ params: { userId } }: { params: { userId: string } }
+) => {
+	try {
+		const { user: loggedInUser } = await validateRequest()
+
+		if (!loggedInUser)
+			return NextResponse.json({ error: 'Unauthorized' }, { status: 401 })
+
+		const user = await db.query.userTable.findFirst({
+			columns: { username: true },
+			with: {
+				followers: {
+					columns: {
+						followerId: true
+					}
+				}
+			},
+			where: eq(userTable.id, userId)
+		})
+
+		if (!user) {
+			return NextResponse.json(
+				{ error: 'User not found' },
+				{ status: 404 }
+			)
+		}
+
+		const data: FollowerInfo = {
+			followers: user.followers.length,
+			isFollowedByUser: !!user.followers.length
+		}
+
+		return NextResponse.json(data)
+	} catch (error: any) {
+		console.error(error.message)
+
+		return NextResponse.json(
+			{ error: 'Internal server error' },
+			{ status: 500 }
+		)
+	}
+}
