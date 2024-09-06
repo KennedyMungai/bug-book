86ea6d4857973985a8a3f967a908b232d773dbdf
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-09-06 15:08:08 +0300

    Added tabs to the index page

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -2,14 +2,25 @@ import PostEditor from '@/components/posts/editor/post-editor'
 import TrendsSidebar from '@/components/trends-sidebar'
 import FollowingFeed from './_components/following-feed'
 import ForYouFeed from './_components/foryou-feed'
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
 
 const HomePage = () => {
 	return (
 		<main className='flex w-full min-w-0 gap-5'>
 			<div className='w-full min-w-0 space-y-5'>
 				<PostEditor />
-				<ForYouFeed />
-				<FollowingFeed />
+				<Tabs defaultValue='for-you'>
+					<TabsList>
+						<TabsTrigger value='for-you'>For You</TabsTrigger>
+						<TabsTrigger value='following'>Following</TabsTrigger>
+					</TabsList>
+					<TabsContent value='for-you'>
+						<ForYouFeed />
+					</TabsContent>
+					<TabsContent value='following'>
+						<FollowingFeed />
+					</TabsContent>
+				</Tabs>
 			</div>
 			<TrendsSidebar />
 		</main>
