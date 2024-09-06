a8fcf44a8a8511646f991d18443327446fb8cab3
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-09-06 12:53:46 +0300

    Added the component containing the following feed

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,5 +1,6 @@
 import PostEditor from '@/components/posts/editor/post-editor'
 import TrendsSidebar from '@/components/trends-sidebar'
+import FollowingFeed from './_components/following-feed'
 import ForYouFeed from './_components/foryou-feed'
 
 const HomePage = () => {
@@ -8,6 +9,7 @@ const HomePage = () => {
 			<div className='w-full min-w-0 space-y-5'>
 				<PostEditor />
 				<ForYouFeed />
+				<FollowingFeed />
 			</div>
 			<TrendsSidebar />
 		</main>
