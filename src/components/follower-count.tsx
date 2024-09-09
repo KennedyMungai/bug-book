c1770634906abf34f43917d1e3177f6304ebcbd1
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-09-09 09:01:47 +0300

    Created the follower count component

---
+++ src/components/follower-count.tsx
@@ -0,0 +1,23 @@
+import { useFollowerInfo } from '@/hooks/useFollowerInfo'
+import { FollowerInfo } from '@/lib/types'
+import { formatNumber } from '@/lib/utils'
+
+type Props = {
+	userId: string
+	initialState: FollowerInfo
+}
+
+const FollowerCount = ({ initialState, userId }: Props) => {
+	const { data } = useFollowerInfo(userId, initialState)
+
+	return (
+		<span>
+			Followers:{' '}
+			<span className='font-semibold'>
+				{formatNumber(data?.followers ?? 0)}
+			</span>
+		</span>
+	)
+}
+
+export default FollowerCount
