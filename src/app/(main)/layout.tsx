b9e5c6d37da995b76bbc0faa5707f49cdd2737ba
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-09-10 14:21:25 +0300

    Set up the basic upload thing code

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,31 +1,32 @@
-import { validateRequest } from '@/auth'
-import { redirect } from 'next/navigation'
-import { ReactNode } from 'react'
-import SessionProvider from './_components/session-provider'
-import NavBar from './_components/navbar'
-import MenuBar from './_components/menu-bar'
+import { validateRequest } from "@/auth";
+import "@uploadthing/react/styles.css";
+import { redirect } from "next/navigation";
+import { ReactNode } from "react";
+import MenuBar from "./_components/menu-bar";
+import NavBar from "./_components/navbar";
+import SessionProvider from "./_components/session-provider";
 
 type Props = {
-	children: ReactNode
-}
+	children: ReactNode;
+};
 
 const MainLayout = async ({ children }: Props) => {
-	const session = await validateRequest()
+	const session = await validateRequest();
 
-	if (!session.user) redirect('/login')
+	if (!session.user) redirect("/login");
 
 	return (
 		<SessionProvider value={session}>
-			<div className='flex flex-col h-full'>
+			<div className="flex flex-col h-full">
 				<NavBar />
-				<div className='max-w-7xl p-5 mx-auto flex w-full grow gap-5'>
-					<MenuBar className='sticky top-[5.25rem] h-fit hidden sm:block flex-none space-y-3 rounded-2xl bg-card px-3 py-5 lg:px-5 shadow-sm xl:w-80' />
+				<div className="max-w-7xl p-5 mx-auto flex w-full grow gap-5">
+					<MenuBar className="sticky top-[5.25rem] h-fit hidden sm:block flex-none space-y-3 rounded-2xl bg-card px-3 py-5 lg:px-5 shadow-sm xl:w-80" />
 					{children}
 				</div>
-				<MenuBar className='sticky bottom-0 flex w-full justify-center gap-5 bg-card p-3 sm:hidden' />
+				<MenuBar className="sticky bottom-0 flex w-full justify-center gap-5 bg-card p-3 sm:hidden" />
 			</div>
 		</SessionProvider>
-	)
-}
+	);
+};
 
-export default MainLayout
+export default MainLayout;
