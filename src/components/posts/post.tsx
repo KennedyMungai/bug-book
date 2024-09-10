4cd239137a9ed162c4f5f7afbdbef6fee39818bf
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-09-10 11:05:56 +0300

    Wrapped the content inside the linkify component

--- src/components/posts/post.tsx
+++ src/components/posts/post.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { useSession } from '@/app/(main)/_components/session-provider'
+import Linkify from "@/components/linkify";
 import UserAvatar from '@/components/user-avatar'
 import { PostData } from '@/lib/types'
 import { formatRelativeDate } from '@/lib/utils'
@@ -8,29 +9,29 @@ import Link from 'next/link'
 import PostMoreButton from './post-more-button'
 
 type Props = {
-	post: PostData
-}
+	post: PostData;
+};
 
 const Post = ({ post }: Props) => {
-	const { user } = useSession()
+	const { user } = useSession();
 
 	return (
-		<article className='rounded-2xl bg-card p-5 space-y-3 shadow-sm group/post'>
-			<div className='flex justify-between gap-3'>
-				<div className='flex flex-wrap gap-3'>
+		<article className="rounded-2xl bg-card p-5 space-y-3 shadow-sm group/post">
+			<div className="flex justify-between gap-3">
+				<div className="flex flex-wrap gap-3">
 					<Link href={`/users/${post.user?.username}`}>
 						<UserAvatar avatarUrl={post.user?.avatarUrl} />
 					</Link>
 					<div>
 						<Link
 							href={`/users/${post.user?.username}`}
-							className='hover:underline block font-medium'
+							className="hover:underline block font-medium"
 						>
 							{post.user?.displayName}
 						</Link>
 						<Link
 							href={`/posts/${post.id}`}
-							className='text-muted-foreground hover:underline block text-sm'
+							className="text-muted-foreground hover:underline block text-sm"
 						>
 							{formatRelativeDate(post.createdAt)}
 						</Link>
@@ -39,15 +40,15 @@ const Post = ({ post }: Props) => {
 				{post.user?.username === user?.username && (
 					<PostMoreButton
 						post={post}
-						className='opacity-0 transition-opacity group-hover/post:opacity-100'
+						className="opacity-0 transition-opacity group-hover/post:opacity-100"
 					/>
 				)}
 			</div>
-			<div className='break-words whitespace-pre-line'>
-				{post.content}
-			</div>
+			<Linkify>
+				<div className="break-words whitespace-pre-line">{post.content}</div>
+			</Linkify>
 		</article>
-	)
-}
+	);
+};
 
 export default Post
