65cbc75262803bc9e5cf6c90589af28f32d9e9e3
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-09-10 11:29:12 +0300

    Created the user-tooltip component

---
+++ src/components/user-tooltip.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import { useSession } from "@/app/(main)/_components/session-provider";
+import {
+	Tooltip,
+	TooltipContent,
+	TooltipProvider,
+	TooltipTrigger,
+} from "@/components/ui/tooltip";
+import { FollowerInfo, UserData } from "@/lib/types";
+import Link from "next/link";
+import { PropsWithChildren } from "react";
+import UserAvatar from "./user-avatar";
+
+interface UserTooltipProps extends PropsWithChildren {
+	user: UserData;
+}
+
+const UserTooltip = ({ user, children }: UserTooltipProps) => {
+	const { user: loggedInUser } = useSession();
+
+	const followerState: FollowerInfo = {
+		followers: user.followers.length,
+		isFollowedByUser: user.followers.some(
+			(follower) => follower.followerId === loggedInUser?.id,
+		),
+	};
+
+	return (
+		<TooltipProvider>
+			<Tooltip>
+				<TooltipTrigger asChild>{children}</TooltipTrigger>
+				<TooltipContent>
+					<div className="flex max-w-80 flex-col gap-3 break-words px-1 py-2.5 md:min-w-52">
+						<div className="flex items-center justify-between gap-2">
+							<Link href={`/users/${user.username}`}>
+								<UserAvatar size={70} avatarUrl={user.avatarUrl} />
+							</Link>
+						</div>
+					</div>
+				</TooltipContent>
+			</Tooltip>
+		</TooltipProvider>
+	);
+};
+
+export default UserTooltip;
